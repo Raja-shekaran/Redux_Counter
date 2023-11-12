c3fc0edba7fb421762c3bd6ea1bc8a8c2c39d8dc
Raja-shekaran <raja1999.rsrs@gmail.com>
2023-11-12 18:06:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import Counter from "./features/counter/Counter";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Counter />
     </div>
   );
 }
 
 export default App;
+
+
+// import {compose, pipe} from 'lodash/fp'
+// import './App.css';
+
+
+// Curring
+
+// // function add(a,b){
+// //   return function(b)
+// //   {
+// //     return a + b
+// //   }
+// // }
+
+// // const add2 = a=> b=> c => a+b+c;
+
+// // console.log(add2(6)(4)(12));
+
+// let input = " Subscribe    "
+
+// const trim = str => str.trim();
+// const wrap = type => str => `<${type}>${str}</${type}>`;
+// const toLowerCase = str => str.toLowerCase();
+
+// //const transform = compose(wrap, toLowerCase, trim)
+// const transform = pipe(trim, toLowerCase, wrap("div"));
+
+// console.log(transform(input));
